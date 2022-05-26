973a7f6d34c60bec9f54ebe87dca4a3210f4f329
Duncan Mak <duncanmak@gmail.com>
2022-05-26 17:27:40 -0400

    Use dynamic imports to exclude lit from bundle output

--- src/hello/main.ts
+++ src/hello/main.ts
@@ -1,30 +1,31 @@
-import "https://cdn.skypack.dev/trusted-types?dts";
-
-// @deno-types="https://cdn.skypack.dev/lit?dts"
-import { LitElement, html } from "https://cdn.skypack.dev/lit";
-import { customElement, property } from "https://cdn.skypack.dev/lit/decorators";
-import { HelloApi } from "./api/api.ts";
 import { isLocal } from '../environment.ts';
 import { MockApi, RealApi } from './api/api.ts';
+const api = isLocal() ? MockApi : RealApi;
 
-const api: HelloApi = isLocal() ? MockApi : RealApi;
+const { LitElement, html } = await import("https://cdn.skypack.dev/lit?dts");
+const { customElement, property } = await import("https://cdn.skypack.dev/lit/decorators");
 
 @customElement("hello-world")
 export class HelloWorld extends LitElement {
 
+  @property({type: String})
+  _text = "";
 
-  @property({ type: String }) text: string = "";
-
-  // TODO: Can Event handlers be async?
-  async onClick(e: Event) {
-    this.text = await api.hello();
-    this.requestUpdate();
+  onClick(e: Event) {
+    console.log(e);
+    this._text = "Clicked";
   }
 
   render() {
     return html`
       <p><button @click="${this.onClick}">Hello</button></p>
-      <p>${this.text}</p>
+      <p>${this._text}</p>
     `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "hello-world": HelloWorld;
   }
 }
