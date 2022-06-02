65a23505314ff3de99866c9ca2b06ddac6976006
Duncan Mak <duncanmak@gmail.com>
2022-06-02 18:25:29 -0400

    Use dynamic import for vaadin-router

--- src/router.ts
+++ src/router.ts
@@ -1,2 +1,2 @@
-import { Router } from "https://cdn.skypack.dev/@vaadin/router";
+const { Router } = await import("https://cdn.skypack.dev/@vaadin/router");
 export const router = new Router(document.getElementById("outlet"));
